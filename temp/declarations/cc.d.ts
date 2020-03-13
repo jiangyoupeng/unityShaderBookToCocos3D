ea2fd254fff3d18aaaaa96554a8f4369feb1e2cf
zhouyoupeng <zhouyoupeng@yy.com>
2020-03-13 18:31:40 +0800

    提交最新的测试项目

--- temp/declarations/cc.d.ts
+++ temp/declarations/cc.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="F:\creator\CocosCreator3D_1_0_4\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
+/// <reference path="G:\CocosCreator\CocosCreator3D_1_0_4\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
