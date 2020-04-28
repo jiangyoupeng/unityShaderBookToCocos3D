6d90744f3c805c6b6714235c8646c3928e0b43d4
zhouyoupeng <zhouyoupeng@yy.com>
2020-04-28 18:32:06 +0800

    提交第8章的例子

--- temp/declarations/cc.d.ts
+++ temp/declarations/cc.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="G:\CocosCreator\CocosCreator3D_1_0_4\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
+/// <reference path="G:\CocosCreator\CocosCreator3D-v1.1.0-win32-042220\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
